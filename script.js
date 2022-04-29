673eff8db543e3c7e3ff497fea16aefd01554568
Habeebability <awoyemi.habeeb@gmail.com>
2022-04-29 14:14:52 +0100

    updates to landing page

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ hamburger.addEventListener("click", () => {
   //   }
   navLinks.classList.toggle("hidden");
 });
+
+let landingProducts = [
+  {
+    name: "Ice Blue",
+    img: "./img/paintbucketsBlue.png",
+    price: 20000,
+  },
+  {
+    name: "Ice Brown",
+    img: "./img/paintbucketsCreame.png",
+    price: 28000,
+  },
+  {
+    name: "Ice Creame",
+    img: "./img/paintbucketsBlue.png",
+    price: 24000,
+  },
+  {
+    name: "Ice Orange",
+    img: "./img/paintbucketsBlue.png",
+    price: 24000,
+  },
+  {
+    name: "Ice Pink",
+    img: "./img/paintbucketsBlue.png",
+    price: 24000,
+  },
+  {
+    name: "Ice White",
+    img: "./img/paintbucketsBlue.png",
+    price: 24000,
+  },
+];
+
+landingProducts.forEach((product) => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div
+  class="product cursor-pointer lg:w-1/3 flex flex-col items-center mb-4"
+>
+  <img src="./img/paintbucketsBlue.png" alt="" />
+  <h3 class="paintTitle">Ice Blue</h3>
+  <h6 class="paint-price">₦20,000</h6>
+</div>
+  `;
+
+  product.appendChild(div);
+});
